2adeb8f3e377a0bde27b2b611c8d47c7b761abff
Artyom Yudin <evomysense@gmail.com>
2022-10-14 01:00:19 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
+const menuBtn = document.querySelector('.menubar'),
+      menuSide = document.querySelector('.mobile-menu'),
+      crossBtn = document.querySelector('.mobile-cross')
+      // crossBtn = window.getComputedStyle(elementCross, ':after')
+
+menuBtn.addEventListener('click', asideMenu)
+function asideMenu() {
+  menuSide.classList.toggle('active-menu')
+}
+
+crossBtn.addEventListener('click', function() {
+  menuSide.classList.remove('active-menu')
+})
+
+document.addEventListener('mouseup', e => {
+  if (!menuSide.contains(e.target)) {
+    menuSide.classList.remove('active-menu')
+  }
+})
+
+$(".phone_mask").mask("+7 (999) 999-99-99");
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 140) {
+    document.querySelector('header').classList.add('fixed_stick')
+  } else {
+    document.querySelector('header').classList.remove('fixed_stick')
+  }
+})
+
 const swiper = new Swiper('.swiper-testimonials', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     autoplay: {
-        delay: 3000
+    delay: 3000
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
     },
   });
 
   const swiper1 = new Swiper('.swiper-partners', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     slidesPerView: 7,
     spaceBetween: 15,
     autoplay: {
-        delay: 3000
+    delay: 3000
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+    pagination: {
+      el: '.swiper-pagination',
     },
     breakpoints: {
         320: {
